996d665ae5a7a27a416de7c1f465e9264193ff73
qmm <qmmhao@sina.com>
2016-06-08 20:28:35 +0800

    perfect

--- addData.js
+++ addData.js
@@ -3,6 +3,7 @@ var maxId = 0;
 function getAttributes(input) {
     var correctInput = {};
     if (input.barcode && input.name && input.unit && input.price) {
+        correctInput.id = ++maxId;
         correctInput.barcode = input.barcode;
         correctInput.name = input.name;
         correctInput.unit = input.unit;
@@ -16,33 +17,21 @@ function judgeType(input) {
     if (attributes) {
         if (typeof(input.barcode) != "string" || typeof(input.name) != "string"
             || typeof(input.unit) != "string" || typeof(input.price) != "number") {
+            maxId--;
             return;
         }
     }
-    console.log(attributes);
     return attributes;
-}
-
-function addId(input) {
-        var Obj = {};
-        Obj.id = ++maxId;
-        Obj.barcode = input.barcode;
-        Obj.name = input.name;
-        Obj.unit = input.unit;
-        Obj.price = input.price;
-       return Obj;
 }
 
 function insertData(req, res) {
     var data = JSON.parse(fs.readFileSync("./data.json"));
     var correctInput;
-        console.log(req.body);
-        correctInput = judgeType(req.body);
-        if (!correctInput) {
-            res.status(400).end();
-            return;
-        }
-    correctInput =  addId(correctInput);
+    correctInput = judgeType(req.body);
+    if (!correctInput) {
+        res.status(400).end();
+        return false;
+    }
     data.push(correctInput);
     fs.writeFile("./data.json", JSON.stringify(data), function (err) {
         if (err) {
