6b8f38a5a424df494750c44d514e9485268783d6
qmm <qmmhao@sina.com>
2016-06-22 16:45:24 +0800

    update

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var fs = require("fs");
+var bodyParser = require("body-parser");
+var router = express();
+
+router.listen(3000);
+router.use(bodyParser.json());
+fs.exists("./data.json", function (exists) {
+    if (!exists) {
+        fs.open("./data.json", "a", function (err, fd) {
+            if (err) {
+                console.log("文件不存在,创建不成功!");
+            }
+            else {
+                fs.writeFileSync("./data.json", JSON.stringify([]));
+            }
+
+        });
+    }
+});
+
+router.post('/products', require("./addData").insertData);
+router.delete('/products/:id', require("./deleteData"));
+router.get('/products/:id', require("./findData").findOne);
+router.get('/products', require("./findData").findAll);
+router.put('/products/:id', require("./updateData"));
